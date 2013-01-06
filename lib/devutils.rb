92c354074cba318bdcb0b8b40a5c67a862bd2162
Gibheer <gibheer@gmail.com>
2013-01-06 11:51:45 +0100

    rewrote the thor task
    The old format worked at first, but created some problems for future
    tasks. With that, it is easier to extend it for future further tasks.

--- lib/devutils.rb
+++ lib/devutils.rb
@@ -1,19 +1,24 @@
 require 'thor'
 require 'thor/group'
 
-class Init < Thor::Group
+class Commands < Thor
   include Thor::Actions
-
-  # needs to be given for a new project
-  argument :project_name
-
-  desc 'initialize a new project'
-  def init
-    Init.source_root(File.expand_path('../../', __FILE__))
+  
+  desc 'init PROJECT_NAME', 'initialize a new project named PROJECT_NAME'
+  method_option :metrics,
+    :default => true,
+    :type    => :boolean,
+    :desc    => 'install tools for code quality analyzing'
+  method_option :docs,
+    :default => true,
+    :type    => :boolean,
+    :desc    => 'install yard for documentation'
+  method_option :guard,
+    :default => true,
+    :type    => :boolean,
+    :desc    => 'install guard for automated testing'
+  def init(project_name)
+    Commands.source_root(File.expand_path('../..', __FILE__))
     directory('template', project_name)
   end
-end
-
-class Commands < Thor
-  register(Init, 'init', 'init PROJECT_NAME', 'initialize a new project')
 end
